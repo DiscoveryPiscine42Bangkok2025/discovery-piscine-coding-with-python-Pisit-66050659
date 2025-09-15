359afbdbff91706d646fb5dfa31ab22fd59e013d
Pisit-66050659 <146307326+Pisit-66050659@users.noreply.github.com>
2025-09-15 14:37:25 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,10 @@
+try:
+  number = int(input("Enter a number: "))
+  if number > 25:
+    print("Error")
+  else:
+    for i in range(number, 26):
+      print(i)
+except ValueError:
+  print("Error")
+
