0ebaf6edb6a15b5204ae2df7d5fb36f3f17d253c
Pisit-66050659 <146307326+Pisit-66050659@users.noreply.github.com>
2025-09-15 14:07:26 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = input("Enter a number: ")
+if number > "0":
+    print("This number is positive.")
+elif number < "0":
+    print("This number is negative.")
+else:
+    print("This number is zero.")
