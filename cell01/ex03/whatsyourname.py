b712453420ef2817aceadef60837e933b9bf499c
Pisit-66050659 <146307326+Pisit-66050659@users.noreply.github.com>
2025-09-15 13:38:55 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+view1 = "Hey, What's your name?"
+chok1 = " : Chok"
+view2 = "And your last name?"
+chok2 = " : Kiattimahaphon"
+view3 = "Nice to meet you, Chok Kiattimahaphon"
+print(view1 + chok1+ "\n" + view2 + chok2 + "\n" + view3)
