806df3d324671d56a6a7f9e4f0de35ea7b09c71a
Pisit-66050659 <146307326+Pisit-66050659@users.noreply.github.com>
2025-09-16 21:06:18 +0700

    Delete cell01/ex04 directory

--- cell01/ex04/round_up.py
+++
@@ -1,9 +0,0 @@
-import math
-
-num = input("Enter a number: ")
-try:
-    number = float(num)
-    rounded_number = math.ceil(number)
-    print(f"The rounded-up integer is: {rounded_number}")
-except ValueError:
-    print("Please enter a valid number.")
