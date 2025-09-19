9e125f5faaaecb435c6a4e4f3d5345091a6e2fea
Pisit-66050659 <146307326+Pisit-66050659@users.noreply.github.com>
2025-09-19 16:58:32 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,6 +1,5 @@
-array = [2, 8, 9, 48, 8, 22, -12, 2]
-array_plus_2 = [i + 2 for i in array if i > 5]
-set_array = set(array)
-
-print(f"{array}")
-print(f"{set_array}")
+number = [2, 8, 9, 48, 8, 22, -12, 2]
+remove_duplicates = [x for x in number if number.count(x) == 1]
+new_u = [x + 2 for x in remove_duplicates]
+print(list(number))
+print(list(new_u))
