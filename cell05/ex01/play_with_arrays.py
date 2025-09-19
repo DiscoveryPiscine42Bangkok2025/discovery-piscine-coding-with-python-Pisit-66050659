d831e75df3c2d5c51a98d76166a3766a8982ed37
Pisit-66050659 <146307326+Pisit-66050659@users.noreply.github.com>
2025-09-19 16:51:27 +0700

    Update play_with_arrays.py

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,5 +1,5 @@
-import sys
-args = sys.argv[1:]
-input_u = input("Enter a list of numbers : ").split()
-args = input_u
-print([int(x) + 2 for x in args])
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+array_plus_2 = [i + 2 for i in array]
+
+print(f"Original array: {array}")
+print(f"New array: {array_plus_2}")
