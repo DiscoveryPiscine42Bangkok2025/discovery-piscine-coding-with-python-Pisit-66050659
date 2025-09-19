fbccb97be9e71c82d8df3a8004f22e21f48fc142
Pisit-66050659 <146307326+Pisit-66050659@users.noreply.github.com>
2025-09-19 16:55:38 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,9 +1,6 @@
-import sys
-args = sys.argv[1:]
-input_u = input("Enter a list of numbers : ").split()
-args = input_u
-numbers = [int(x) for x in input_u]
-remove_duplicates = [x for x in numbers if numbers.count(x) == 1]
-new_u = [x + 2 for x in remove_duplicates]
-print(list(numbers))
-print(list(new_u))
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+array_plus_2 = [i + 2 for i in array if i > 5]
+set_array = set(array)
+
+print(f"{array}")
+print(f"{set_array}")
